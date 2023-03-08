b8e4f049cf20183971777955889787f9be40aca8
ashishsecrets <ashishsecrets@users.noreply.github.com>
2023-03-08 19:18:29 +0530

    added another screen

---
+++ freeswitchandroid/src/main/java/com/example/freeswitchandroid/CallsHistory.java
@@ -0,0 +1,14 @@
+package com.example.freeswitchandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CallsHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calls_history);
+    }
+}

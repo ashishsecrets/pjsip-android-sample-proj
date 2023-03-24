11a35212fc3b7ab2220eeab2722ab3cb3937ea1e
ashishsecrets <ashishsecrets@users.noreply.github.com>
2023-03-24 12:47:31 +0530

    Added crash handling with an extra Application class

---
+++ freeswitchandroid/src/main/java/com/example/freeswitchandroid/App.java
@@ -0,0 +1,33 @@
+package com.example.freeswitchandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Application;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+
+public class App extends Application {
+        @Override
+        public void onCreate() {
+            super.onCreate();
+            Thread.setDefaultUncaughtExceptionHandler(
+                    new Thread.UncaughtExceptionHandler() {
+                        @Override
+                        public void uncaughtException(Thread thread, Throwable e) {
+                            handleUncaughtException(thread, e);
+                        }
+                    });
+        }
+
+    private void handleUncaughtException (Thread thread, Throwable e) {
+
+        // The following shows what I'd like, though it won't work like this.
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+
+        // Add some code logic if needed based on your requirement
+    }
+}
